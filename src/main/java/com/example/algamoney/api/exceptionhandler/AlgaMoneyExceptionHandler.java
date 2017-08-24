65173c1c4a11b4b122a25daa387ef20ba9f8355f
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-08-24 18:05:31 -0300

    Tratando exception EmptyResultDataAccessException com mensagem ao usuario

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
@@ -73,8 +73,12 @@ public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 	 */
 	@ExceptionHandler({EmptyResultDataAccessException.class})
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public void handleEmptyResultDataAccessException(RuntimeException ex) {
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
 		
+		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros , new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	/**
