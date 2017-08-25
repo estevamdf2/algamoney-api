a7a3dd9c9e3dd59af00d67b0dfd21b849c62d421
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-08-25 18:15:48 -0300

    Implementando DELETE e PUT para categoria

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -10,15 +10,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.repository.CategoriaRepository;
+import com.example.algamoney.api.service.categoriaService;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
 
@@ -40,6 +44,9 @@ public class CategoriaResource {
 	 */
 	@Autowired 
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private categoriaService categoriaService;
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -80,5 +87,18 @@ public class CategoriaResource {
 		}
 
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo ){
+		categoriaRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria){
+		Categoria categoriaSalva = categoriaService.atualizar(codigo,categoria);
+		return ResponseEntity.ok(categoriaSalva);
+		
 	}
 }
