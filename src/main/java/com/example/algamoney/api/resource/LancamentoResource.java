38e08d55e893557cc7f2c323297e86dfd90a57a6
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-08-29 16:58:49 -0300

    Cap. 5 A3 - desafio cadastrar categoria

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -2,13 +2,22 @@ package com.example.algamoney.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.algamoney.api.model.Lancamento;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 
 
@@ -19,6 +28,10 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 	
+	//Eventos
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Lancamento> listar(){
 		return lancamentoRepository.findAll();
@@ -28,6 +41,22 @@ public class LancamentoResource {
 	public Lancamento buscarPorCodigo(@PathVariable Long codigo){
 		
 		return lancamentoRepository.findOne(codigo);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		
+		/*
+		 * Parametros
+		 * this - > objeto que gerou o evento (PessoaResource)
+		 * response ->
+		 * codigo -> identificador da pessoaSalva
+		 */
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		//Inserir o recurso salvo o body
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 }
