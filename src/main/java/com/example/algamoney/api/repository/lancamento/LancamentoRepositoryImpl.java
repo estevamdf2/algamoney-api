5c947d1890a44b3d6c3aade93340b2fd0ecd9cbf
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-09-05 16:38:09 -0300

    Cap 5. Aula 9 implementação da paginação

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -11,6 +11,9 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.example.algamoney.api.model.Lancamento;
@@ -24,7 +27,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 	private EntityManager manager;
 	
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 		
@@ -39,9 +42,13 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		criteria.where(predicates);
 		TypedQuery<Lancamento> query = manager.createQuery(criteria);
 		
-		return query.getResultList();
+		adicionarRestricoesDePaginacao(query, pageable);
+		
+		//Retornar um objeto PageAble
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter)) ;
 	}
 
+	
 	private Predicate[] CriarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
 			Root<Lancamento> root) {
 
@@ -73,6 +80,42 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		 * uma lista de array
 		 */
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+	/**
+	 * Define o total de registros por página e 
+	 * especifica qual e o primeiro registro para 
+	 * uma determinada página.
+	 * @param query
+	 * @param pageable
+	 */
+	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
+		
+		query.setFirstResult(primeiroRegistroDaPagina);
+		query.setMaxResults(totalRegistrosPorPagina);
+	}
+	
+	/**
+	 * Calcula a quantidade de elementos
+	 * totais retornado pela consulta
+	 * @param lancamentoFilter
+	 * @return
+	 */
+	private Long total(LancamentoFilter lancamentoFilter) {
+
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		Predicate[] predicates = CriarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		criteria.select(builder.count(root));
+		
+		return manager.createQuery(criteria).getSingleResult();
 	}
 
 }
--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,12 +2,13 @@ package com.example.algamoney.api.repository.lancamento;
 
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.algamoney.api.model.Lancamento;
 
 //Responsável pela consulta da Query do SpringDateJPA
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
